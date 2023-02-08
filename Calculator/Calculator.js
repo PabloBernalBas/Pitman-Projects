98c6dfb84528f610a083893e7d61777f3351977e
PabloBernalBas <116829980+PabloBernalBas@users.noreply.github.com>
2023-02-08 18:13:25 +0000

    Add files via upload

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -11,13 +11,13 @@ const Calculator = {
 };
 
 // this modifies values each time a button is clicked
-function Input_Digit (digit) {
-    const {Display_Value, Wait_Second_Operand} = Calculator;
+function Input_Digit(digit) {
+    const { Display_Value, Wait_Second_Operand } = Calculator;
     // we are checking to see if wait Second operand is true and set
     // Display Value to the key that was clicked.
     if (Wait_Second_Operand === true) {
     Calculator.Display_Value = digit; 
-    Calculator.Wait_Second_Operand === false;
+    Calculator.Wait_Second_Operand = false;
     } else {
     // this overwrites Display-Value if the current value is 0
     // otherwise it adds onto it 
@@ -38,20 +38,20 @@ function Input_Decimal(dot) {
 }
 
 //this section handles operators
-function Handle_Operator (Next_Operator) {
-    const { First_Operand, Display_Value, operator } = Calculator 
+function Handle_Operator(Next_Operator) {
+    const { First_Operand, Display_Value, operator } = Calculator; 
     //when an operator key is pressed, we convert the current number 
     //displayed on the screen to a number and then store the result in
     //calculator.First operand if it doesn't already exist 
     const Value_of_Input = parseFloat(Display_Value);
     //checks if an operator already exists and if Wait_Second_Operand is true.
     //then updates the operator and exits from the function
-    if (operator && Calculator.Wait__Second_Operand) {
+    if (operator && Calculator.Wait_Second_Operand) {
         Calculator.operator = Next_Operator;
         return;
     }
-    if (First_Operand = null) { 
-        calculator.First_Operand = Value_of_Input;
+    if (First_Operand == null) { 
+        Calculator.First_Operand = Value_of_Input;
     } else if (operator) { //checks if an operator already exists 
         const Value_Now = First_Operand || 0; 
         //If operator exists, property lookup is performed for the operator
@@ -65,7 +65,7 @@ function Handle_Operator (Next_Operator) {
         Calculator.Display_Value = parseFloat(result); 
         Calculator.First_Operand = parseFloat(result);
     }
-    calculator.Wait_Second_Operand = true;
+    Calculator.Wait_Second_Operand = true;
     Calculator.operator = Next_Operator;
 }
 
@@ -77,16 +77,16 @@ const Perform_Calculation = {
     '=': (First_Operand, Second_Operand) => Second_Operand
 };
 
-function Calculator__Reset() {
-    Calculator.Display_Value = 0;
+function Calculator_Reset() {
+    Calculator.Display_Value = '0';
     Calculator.First_Operand = null; 
     Calculator.Wait_Second_Operand = false;
     Calculator.operator = null;
 }
 
 // this function updates the screen with the contents of Display Value
-function Update_Display () {
-    const display = document.querySelector('.calculator-keys'); 
+function Update_Display() {
+    const display = document.querySelector('.calculator-screen'); 
     display.value = Calculator.Display_Value;
 }
 
@@ -96,20 +96,20 @@ const keys = document.querySelector('.calculator-keys');
 keys.addEventListener('click', (event) => {
     //the target variable is an object that represents the element 
     // that was clicked
-    const {target} = event; 
+    const { target } = event; 
     // if the element that was clicked on is not a button, exit the function
     if (!target.matches('button')) { 
         return;
     }
 
-    if (target.classList.contains ('operator')) { 
-        Handle_Operator (target.value);
+    if (target.classList.contains('operator')) { 
+        Handle_Operator(target.value);
         Update_Display(); 
         return;
     }
 
     if (target.classList.contains('decimal')) { 
-        Input_Decimal (target.value);
+        Input_Decimal(target.value);
         Update_Display(); 
         return;
     }
